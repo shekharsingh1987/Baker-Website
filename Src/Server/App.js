7ab9f39a8a23b5e93ae3c07fd746bc541d6a0d86
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2016-09-01 20:01:59 +0530

    Added the Nuget package

--- Src/Server/App.js
+++ Src/Server/App.js
@@ -1,12 +1,17 @@
 //Importing the required modules 
 var http=require("http");
+var file=require("fs");
 
+var Totalrequests=0;
 
 //Server code
 http.createServer(function (request,response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    response.end('Hello Shekhar kumar! nice work');
+    Totalrequests+=1;
+    //response.end(Totalrequests);
+   var fileData= file.readFileSync('ReadMe.txt');
+    response.end(fileData);   
+    console.log(Totalrequests/2);     
     
 }).listen(81);
 
