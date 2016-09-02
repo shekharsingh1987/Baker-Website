2f93e3b7be56df239df59ed6059b989231378e4e
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2016-09-02 13:08:19 +0530

    Added event handler for Js

--- Src/Server/App.js
+++ Src/Server/App.js
@@ -1,19 +1,36 @@
 //Importing the required modules 
 var http=require("http");
 var file=require("fs");
-
+var events=require("events");
 var Totalrequests=0;
 
-//Server code
-http.createServer(function (request,response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    Totalrequests+=1;
-    //response.end(Totalrequests);
-   var fileData= file.readFileSync('ReadMe.txt');
-    response.end(fileData);   
-    console.log(Totalrequests/2);     
-    
+//declare events
+var eventEmitter=new events.EventEmitter();
+
+
+//Create event handler
+var responseHandler=function Respond() {     
+    console.log('Connected succesfully');
+};
+
+var logConsoleHandler=function LogConsole() {
+    console.log('Server running at http://127.0.0.1:81/');
+}
+
+//console.log(Totalrequests/2);     
+eventEmitter.on('logging',logConsoleHandler); 
+//Bind these two handler
+eventEmitter.on('connection',responseHandler);
+
+http.createServer(function (request,response) { 
+    response.writeHead(200, {'Content-Type': 'text/plain'}); 
+    var fileData= file.readFileSync('ReadMe.txt');
+    response.end(fileData);    
+    eventEmitter.emit('connection');
+ //   Totalrequests+=1;    
+  
 }).listen(81);
 
-console.log('Server running at http://127.0.0.1:81/');
+
+ eventEmitter.emit('logging');
 
