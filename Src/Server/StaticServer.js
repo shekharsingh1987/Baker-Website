d821db0c3d519d222468bd0d93821a78a1601014
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2016-09-09 18:40:39 +0530

    Added html page and managed the routing

--- Src/Server/StaticServer.js
+++ Src/Server/StaticServer.js
@@ -3,9 +3,10 @@ var app=express();
 
 //You can acces everything inside this folder
 app.use(express.static('Public'));
+app.use(express.static('Views'));
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.sendFile(__dirname+"/"+"Views"+"/"+"Index.html");
 });
 
 var server = app.listen(81, function () {
